36162306b8c8ebe7c582f7031540d86d77db7b83
enaciodabasi <enaciodabasi@outlook.com>
2023-02-27 10:23:44 +0300

    Tested start function of the controller base class.

--- examples/epos4_example_main.cpp
+++ examples/epos4_example_main.cpp
@@ -9,6 +9,6 @@ int main()
     //auto inf = ethercat_interface::utilities::parse_config_file("/home/naci/Projects/igh_master/examples/epos4_config.yaml", "EPOS4_0");
     //inf.toString();
 
-    
+    m.start();
 
 }
