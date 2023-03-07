7a6d134813ab16a0c99ee05cac3a26dab674694e
enaciodabasi <enaciodabasi@outlook.com>
2023-03-07 10:16:36 +0300

    Added Master base class for a higher level of abstraction and to share the slave config between different slaves.

---
+++ include/master.hpp
@@ -0,0 +1,33 @@
+/**
+ * @file master.hpp
+ * @author Eren Naci Odabasi (enaciodabasi@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-03-07
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef MASTER_HPP
+#define MASTER_HPP
+
+#include "ecrt.h"
+
+namespace ethercat_interface
+{
+    namespace master
+    {
+        class Master
+        {
+            public:
+
+            protected:
+
+            private:
+
+        };
+    }
+}
+
+#endif // MASTER_HPP
