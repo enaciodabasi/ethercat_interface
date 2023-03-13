a2cd8e68bff8a703247b7f73e575e215d0bf91f4
enaciodabasi <enaciodabasi@outlook.com>
2023-03-13 13:25:40 +0300

    Removed EPOS4 example with the controller Method.

--- examples/slave_example.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "slave_example.hpp"
-
-EPOS4::EPOS4(const std::string& slave_name, ethercat_interface::utilities::SlaveInfo slave_info, ethercat_interface::Offset* offset)
-    : ethercat_interface::slave::Slave(slave_name, slave_info, offset, true)
-{
-    std::cout << "Creating " << m_SlaveName << " Slave." << std::endl;
-}
