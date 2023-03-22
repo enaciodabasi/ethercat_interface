3387b55aa524d6451f16d72a77b5bc89412be6e7
enaciodabasi <enaciodabasi@outlook.com>
2023-03-22 14:42:58 +0300

    Now using bit masking during status control.

--- src/state.cpp
+++ src/state.cpp
@@ -16,12 +16,11 @@ namespace ethercat_interface
 
         bool isStatusCorrect(SlaveStatus current_status, StatusType target_status)
         {
-            std::string currentStatusStr = std::bitset<8>((uint8_t)(current_status & 0x00FF)).to_string();
+            /* std::string currentStatusStr = std::bitset<8>((uint8_t)(current_status & 0x00FF)).to_string();
             std::string targetStatusStr = std::bitset<8>((uint8_t)(getStatusValue(target_status) & 0x00FF)).to_string();
 
             if(currentStatusStr.size() != targetStatusStr.size())
             {   
-                // LOG
                 return false;
             }
 
@@ -42,6 +41,22 @@ namespace ethercat_interface
                 }
             }
 
+            return true; */
+
+            auto skip_indexes = StatusHelper::getNotImportantBits(target_status);
+
+            uint8_t first = (uint8_t)(current_status & 0x00FF);
+            uint8_t second = (uint8_t)(getStatusValue(target_status) & 0x00FF);
+
+            uint8_t mask = 1;
+            for (int i = 0; i < 8; i++) {
+              if (std::find(skip_indexes.begin(), skip_indexes.end(), i) == skip_indexes.end()) {
+                if ((current_status & mask) != (getStatusValue(target_status) & mask)) {
+                  return false;
+                }
+              }
+              mask <<= 1;
+            }
             return true;
         }
     }
