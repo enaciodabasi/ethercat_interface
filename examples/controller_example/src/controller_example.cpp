04e4ae28445f6b5d382d9ae921a64705ed5797c2
enaciodabasi <enaciodabasi@outlook.com>
2023-04-27 17:00:00 +0300

    Both DC slaves in OP mode.

--- examples/controller_example/src/controller_example.cpp
+++ examples/controller_example/src/controller_example.cpp
@@ -1,5 +1,12 @@
 #include "../include/controller_example.hpp"
 
+bool run = true; 
+
+void shutdown_ec_loop(int sig)
+{
+    run = false;
+}
+
 ControllerExample::ControllerExample(const std::string& config_file_path)
     : Controller(config_file_path)
 {
@@ -8,7 +15,60 @@ ControllerExample::ControllerExample(const std::string& config_file_path)
 
 void ControllerExample::cyclicTask()
 {   
-    
+    clock_gettime(m_DcHelper.clock, &m_DcHelper.wakeupTime);
+    while(run)
+    {
+        this->setTaskWakeUpTime();
+        sleep_task(
+            m_DcHelper.clock,
+            TIMER_ABSTIME,
+            &m_DcHelper.wakeupTime,
+            NULL
+        );
+
+        m_Master->setMasterTime(timespecToNanoSec(m_DcHelper.wakeupTime));
+
+        m_Master->receive("domain_1");
+
+        m_Master->updateDomainStates();
+        m_Master->updateMasterState();
+        m_Master->updateSlaveStates();
+        /*
+            ********************************
+            ********** Loop Logic **********
+            ********************************
+        */  
+
+       bool slavesEnabled = m_Master->enableSlaves();
+        this->m_Master->write<int8_t>(
+                "domain_1",
+                "EL7221_9015_0",
+                "op_mode",
+                0x09
+        );
+        this->m_Master->write<int8_t>(
+            "domain_1",
+            "EL7221_9015_1",
+            "op_mode",
+            0x09
+        );
+
+       if(slavesEnabled)
+            std::cout << "aaaaaaaaaaaaaaaaaa\n";
+
+
+       /*
+            ********************************
+            ********** End of Loop Logic **********
+            ********************************
+       */
+
+        clock_gettime(m_DcHelper.clock, &m_DcHelper.currentTime);
+        m_Master->syncMasterClock(timespecToNanoSec(m_DcHelper.currentTime), m_DcHelper);
+        m_Master->send("domain_1");
+
+
+    }
 }
 
 void ControllerExample::startTask()
@@ -22,6 +82,8 @@ void ControllerExample::startTask()
 
 int main()
 {
+    signal(SIGTERM, shutdown_ec_loop);
+    signal(SIGINT, shutdown_ec_loop);
     std::unique_ptr<ControllerExample> cont = std::make_unique<ControllerExample>("/home/naci/Projects/igh_master/examples/controller_example/config.yaml");
     
     if(!cont->setup())
