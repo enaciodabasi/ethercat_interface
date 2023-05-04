d4fbfa7e4cefc3fc107b06b0c8235af23a5d2607
enaciodabasi <enaciodabasi@outlook.com>
2023-05-04 09:09:57 +0300

    Initial commit for slave configuration GUI.

---
+++ ethercat_config_gui/src/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @author Eren Naci Odabasi (enaciodabasi@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-05-04
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "ethercat_config_gui/main_window.hpp"
+
+int main(int argc, char** argv)
+{
+
+
+    return 0;
+}
